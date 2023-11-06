a08ceb97ee9607267c84020ee868c4b40726f2ba
Sviatoslav Bobryshev <wstswsb@gmail.com>
2023-11-06 21:04:35 +0300

    [cli] feat: add progress bar and errors format message

--- src/cli/app.py
+++ src/cli/app.py
@@ -1,7 +1,15 @@
 import asyncio
 from pathlib import Path
 
+import typer
 from rich import print as rich_print
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeElapsedColumn,
+)
 from typer import Typer
 
 from src.tasks.check_hostname import CheckHostnameTask
@@ -14,23 +22,46 @@ app = Typer()
 
 @app.command()
 def main(tasks_path: Path):
-    asyncio.run(async_main(tasks_path))
+    try:
+        asyncio.run(async_main(tasks_path))
+    except Exception as e:
+        exception_type = type(e).__name__
+        error_message = str(e)
+        formatted_exception = f"[bold red]{exception_type}:[/] {error_message}"
+        rich_print(formatted_exception)
+        raise typer.Exit(1)
 
 
 async def async_main(tasks_path: Path):
+    rich_print(f"[cyan]Collecting tasks...[/]")
     loader = TasksLoader(
         task_name_to_class={
             "check_hostname": CheckHostnameTask,
             "check_ping": CheckPingTask,
         }
     )
-    runner = Runner(tasks=loader.load(tasks_path))
-    for completed_coro in runner.run():
-        result = await completed_coro
-        if not result.errors:
-            rich_print(f"[green][+] Task {result.description}[/]")
-        else:
-            rich_print(f"[red][-] Task {result.description} {result.errors}[/]")
+    tasks = loader.load(tasks_path)
+    rich_print(f"[bold cyan]{len(tasks)} tasks collected[/]")
+    runner = Runner(tasks)
+
+    progress_bar = Progress(
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TextColumn("•"),
+        TimeElapsedColumn(),
+    )
+    with progress_bar as progress:
+        task = progress.add_task(description="", total=len(tasks))
+        for index, completed_coro in enumerate(runner.run()):
+            result = await completed_coro
+            if not result.errors:
+                rich_print(f"[green][+] Task {result.description}[/]")
+            else:
+                rich_print(
+                    f"[red][-] Task {result.description}\t| {','.join(result.errors)}[/]"
+                )
+            progress.update(task, completed=index + 1)
 
 
 if __name__ == "__main__":
